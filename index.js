0919c953f7cccf50014f8cfed34e2c85b2838eb8
Emanueljuarez <zerauleuname@gmail.com>
2022-01-12 12:23:55 -0600

    Agregamos propiedades y ciclos

--- index.js
+++ index.js
@@ -18,27 +18,63 @@ console.log(mexico.comida)
 console.log(mexico["comida"])
 
 const olympicRecords = {
-    athletics100Men: "Justin Gatlin",
-    athleticsLongJumpMen: "Mike Powel",
-    swiming200Men: "Michael Phelps",
-    "swimming 200 Women": "Ariarne Titmus"
-
+	athletics100Men: "Justin Gatlin",
+	athleticsLongJumpMen: "Mike Powel",
+	swimming200Men: "Michael Phelps",
+	"swimming 200 Women": "Ariarne Titmus"
 }
 
 console.log(olympicRecords.athletics100Men)
+console.log(olympicRecords["athletics100Men"])
+
 console.log(olympicRecords["swimming 200 Women"])
 
-//AGREGAR PROPIEDADES
+// B. AGREGAR PROPIEDADES
 
-const refresco= {
-    nombre: "Coca cola",
-    color: "Oscuro",
-    precio: 12,
-    tamano: ["600, 1000"]
+const refresco = {
+	nombre: "Coca cola",
+	color: "Oscuro",
+	precio: 12,
+	tamanos: [600, 1000]
 }
 
 refresco.envase = "Plástico"
 
 console.log("Objeto de refresco:", refresco)
-refresco["competidor"] = "pepsi"
+
+refresco["competidor"] = "Pepsi"
+
 console.log(refresco)
+
+
+// C. PROPIEDADES Y CICLOS
+
+// OBJECT.KEYS()
+// Método que permite convertir las propiedades de un objeto en elementos de un arreglo.
+
+const arrRefresco = Object.keys(refresco)
+
+console.log(arrRefresco)
+
+// OBJECT.VALUES()
+// Método que permite convertir los valores de un objeto en elementos de un arreglo.
+
+const arrRefrescoValores = Object.values(refresco)
+
+console.log("arrRefrescoValores", arrRefrescoValores)
+
+// for ... in
+
+// for (VARIABLE KEY in OBJECT)
+for (let key in refresco ) {
+	// console.log(`La propiedad es: ${key}`)
+
+	// if(key === "competidor") console.log("Encontré la prop competidor")
+
+	console.log(`La propiedad es ${key} y su valor es ${refresco[key]}`)
+
+
+}
+
+
+
